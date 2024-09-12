20e8fc707ca49576680de28e8178080e66e2289c
H <h@Hs-MacBook-Pro-2.local>
2024-09-12 15:49:25 -0400

    Update with new changes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import the necessary Firebase modules
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +14,29 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Storage
+const storage = getStorage(app);
+
+export { storage };
+// Import the necessary Firebase modules
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqUhaVsDUvbl4CWeUGNiUIGX3mk4rx0LI",
+  authDomain: "kem-soaw.firebaseapp.com",
+  projectId: "kem-soaw",
+  storageBucket: "kem-soaw.appspot.com",
+  messagingSenderId: "816445607770",
+  appId: "1:816445607770:web:1e5751b7d50d656ee86d63"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Storage
+const storage = getStorage(app);
+
+export { storage };
