2bfa9bf17cc6a37266a49e0f4b8702bb59875575
H <h@Hs-MacBook-Pro-2.local>
2024-09-12 15:53:44 -0400

    Update with new changes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -19,24 +19,3 @@ const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
 export { storage };
-// Import the necessary Firebase modules
-import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBqUhaVsDUvbl4CWeUGNiUIGX3mk4rx0LI",
-  authDomain: "kem-soaw.firebaseapp.com",
-  projectId: "kem-soaw",
-  storageBucket: "kem-soaw.appspot.com",
-  messagingSenderId: "816445607770",
-  appId: "1:816445607770:web:1e5751b7d50d656ee86d63"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Storage
-const storage = getStorage(app);
-
-export { storage };
